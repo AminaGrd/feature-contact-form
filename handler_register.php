3e4bcfa2e183b3a87b1c47d524b513f81423574e
AminaGrd <amina.giraud@gmail.com>
2022-05-12 16:47:19 +0200

    register and confirmation

--- handler_register.php
+++ handler_register.php
@@ -6,9 +6,26 @@
     && isset( $_POST['data-confirmation'] ) && !empty( $_POST['data-confirmation'])) {
 
         if ($_POST['data-password']==$_POST['data-confirmation']) {
-            echo "ok";
+            $username = strip_tags($_POST['data-username']);
+            $mail = strip_tags($_POST['data-mail']);
+            $unencrypted_password = strip_tags($_POST['data-password']);
+            $encrypted_password = password_hash($unencrypted_password, PASSWORD_DEFAULT);
+            //echo $unencrypted_password;
+            //echo "<br>";
+            //echo $encrypted_password;
+            require_once("db_connect.php");
+            $sql='INSERT INTO `tbl_users` (`user_username`, `user_mail`, `user_password`) VALUES (:user_username, :user_mail, :user_password);';
+            $query = $dbh->prepare($sql);
+            $query->bindValue(':user_username', $username, PDO::PARAM_STR);
+            $query->bindValue(':user_mail', $mail, PDO::PARAM_STR);
+            $query->bindValue(':user_password', $encrypted_password, PDO::PARAM_STR);
+            $query->execute();
+    
+            $_SESSION["message"] = "Vous êtes enregistré !";
+            header("Location: index.php");
+            
         }else{
-            echo "Password don't match...";
+            echo "Password don't match... UwU";
         }
 
     }else{
