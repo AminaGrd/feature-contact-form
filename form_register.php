2e73f38978fcff3ddc1d5bf967d8f5dae44fee9b
AminaGrd <amina.giraud@gmail.com>
2022-05-12 16:19:31 +0200

    requires a value

---
+++ form_register.php
@@ -0,0 +1,27 @@
+<?php include("include_header.php") ?>
+
+<form action="handler_register.php" method="post">
+
+    <p>
+        <label for="input-username">Name :</label>
+        <input type="text" name="data-username" id="input-username">
+    </p>
+    <p>
+        <label for="input-mail">Mail :</label>
+        <input type="email" name="data-mail" id="input-mail">
+    </p>
+    <p>
+        <label for="input-password">Mot de passe :</label>
+        <input type="password" name="data-password" id="input-password">
+    </p>
+    <p>
+        <label for="input-confirmation">Confirmation :</label>
+        <input type="password" name="data-confirmation" id="input-confirmation">
+    </p>
+    <p>
+        <input type="submit" value="Enregister">
+    </p>
+
+</form>
+
+<?php include("include_footer.php") ?>
