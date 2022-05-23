f195ff3d50a97934c42ffeb3eaae7c47206f2b4b
AminaGrd <amina.giraud@gmail.com>
2022-05-23 08:59:51 +0200

    finish

---
+++ showHide.js
@@ -0,0 +1,30 @@
+let hideField = document.getElementById("hideField");
+let showPassword = document.getElementById("showPassword");
+
+/*showPassword.onmousedown = function(){
+    hideField.type = "text";
+    showPassword.innerText ="Relacher pour cacher";
+}
+showPassword.onmouseup = function(){
+    hideField.type = "password";
+    showPassword.innerText ="Maintenir pour afficher";
+}
+//cacher si click and drag
+    showPassword.ondrag = function(){
+        hideField.type = "password";
+        showPassword.innerText ="Maintenir pour afficher";
+    } */
+
+    let btnshow = false;
+
+    showPassword.onclick = function(){
+        btnshow = !btnshow;
+        if (btnshow) {
+            hideField.type = "text";
+            showPassword.innerText ="Cliquer pour cacher";
+        } else{
+            
+        hideField.type = "password";
+        showPassword.innerText ="Cliquer pour afficher";
+        }
+    }
