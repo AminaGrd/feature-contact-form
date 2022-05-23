0ddd46d7b6a44285a740bb67391af392b90a049e
AminaGrd <amina.giraud@gmail.com>
2022-05-23 08:53:41 +0200

    js button

--- form_login.php
+++ form_login.php
@@ -2,15 +2,17 @@
 
 <form action="handler_login.php" method="post">
 <p>
-    <label for="">Nom :</label>
+    <label for="">Nom : </label>
     <input type="text" name="data-username">
 </p>
 <p>
-    <label for="">Mot de passe:</label>
-    <input type="password" name="data-password">
+    <label for="">Mot de passe : </label>
+    <input type="password" name="data-password" id="hideField">
+    <a href="#" id="showPassword">Maintenir pour afficher</a>
 </p>
 <p>
-    <input type="submit" value="Se connnecter">
+    <input type="submit" value="Se connecter">
 </p>
 </form>
+<script src="showHide.js"></script>
 <?php include("include_footer.php") ?>
